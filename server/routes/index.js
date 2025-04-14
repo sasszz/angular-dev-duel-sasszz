5738478cc36e4164a97c6e87b9e2833e564a5e31
sasszz <luchevreuil@gmail.com>
2025-04-14 12:18:57 -0700

    token

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import axios from "axios";
 import validate from "express-validation";
 
-import token from "../../token-setup";
+import token from "../../token";
 
 import validation from "./validation/index";
 import userMapper from "../services/userService";
