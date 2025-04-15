245443fd5db581a6c63d721dd77fed21d8f49476
sasszz <luchevreuil@gmail.com>
2025-04-14 18:23:44 -0700

    update api link in client

--- client/src/user.service.ts
+++ client/src/user.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+import { environment } from './environments/environment';
 
-const inspectUserUrl = 'http://localhost:3000/api/user/';
-const duelUsersUrl = 'http://localhost:3000/api/users?';
+const apiBase = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   async inspectUser(username = 'andrew') {
-    let data = await this.http.get(inspectUserUrl + username).toPromise();
+    const url = `${apiBase}/user/${username}`;
+    const data = await firstValueFrom(this.http.get(url));
     console.log(data);
     return data;
   }
 
   async duelUsers(user1 = 'fabpot', user2 = 'andrew') {
-    let data = await this.http.get(duelUsersUrl + `username=${user1}&username=${user2}`).toPromise();
+    const url = `${apiBase}/users?username=${user1}&username=${user2}`;
+    const data = await firstValueFrom(this.http.get(url));
     console.log(data);
     return data;
   }
-
 }
